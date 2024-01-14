5a2a1acaff12f3292c1e599abbdc16182a6437d6
Игорь Бабин <igorbabin2002@gmail.com>
2024-01-14 19:29:29 +0500

    Added template for email sender

---
+++ Artisan.Next/EmailSender/YandexSmtpEmailSender.cs
@@ -0,0 +1,22 @@
+﻿using Artisan.Next.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Artisan.Next.EmailSender;
+
+public class YandexSmtpEmailSender : IEmailSender<ApplicationUser>
+{
+    public Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode)
+    {
+        throw new NotImplementedException();
+    }
+}
