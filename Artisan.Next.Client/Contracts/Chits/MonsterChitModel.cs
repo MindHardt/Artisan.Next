4859a06e85314812072c32474138d1da393e1f82
Игорь Бабин <igorbabin2002@gmail.com>
2024-01-24 13:27:28 +0500

    Updated main.yml, added stats and editor mode

--- Artisan.Next.Client/Contracts/Chits/MonsterChitModel.cs
+++ Artisan.Next.Client/Contracts/Chits/MonsterChitModel.cs
@@ -8,6 +8,7 @@ public record MonsterChitModel
     public required string Name { get; set; }
     public required HealthModel Health { get; set; }
     public required CharacteristicsModel Characteristics { get; set; }
+    public required StatsModel Stats { get; set; }
     public required string ImageUrl { get; set; }
     public required Alignment Alignment { get; set; }
 }
---
+++ Artisan.Next.Client/Contracts/Chits/StatsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Artisan.Next.Client.Contracts.Chits;
+
+public record StatsModel
+{
+    public required int Fort { get; set; }
+    public required int Reac { get; set; }
+    public required int Will { get; set; }
+    public required int Conc { get; set; }
+    public required int Perc { get; set; }
+    public required int Init { get; set; }
+}
